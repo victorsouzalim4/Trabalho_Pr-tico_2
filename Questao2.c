ddf1601d4f4939f82ecc1c400e18fd9370b42d72
Victor Souza Lima <you@example.com>
2024-04-12 17:17:00 -0300

    Criação das structs referentes aos elementos dos objetos do arquivo .csv

---
+++ Questao2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct{
+
+} DateTime;
+
+typedef struct{
+
+} Lista;
+
+typedef struct{
+
+    char id[100];
+    char name[100];
+    Lista alternate_names;
+    char house[100];
+    char ancestry[100];
+    char species[100];
+    char patronus[100];
+    bool hogwartsStaff;
+    bool hogwartsStudent;
+    char actorName[100];
+    bool alive;
+    DateTime dateOfBirth;
+    int yearOfBirth;
+    char  eyeColour[100];
+    char  gender[100];
+    char  hairColour[100];
+    bool wizard;
+
+}Personagem;
+
+int main(){
+
+}
